625d2f0650ecf49edaa83b48469dd68c6559db17
TNT-ma <114092831+TNT-ma@users.noreply.github.com>
2022-12-01 11:51:02 +0800

    Update app.php

--- src/config/plugin/webmans/tntjwt/app.php
+++ src/config/plugin/webmans/tntjwt/app.php
@@ -1,6 +1,8 @@
 <?php
 return [
-    'enable' => true,
-    'key' => '123456',//key
-    'field' => ["id","username"]//允许的字段 
+    'enable' => true,//启用
+    'key' => '123456',//加密·密钥
+    'field' => ["id","username"],//允许存入的字段 
+    'iss' => 'http://www.baidu.com',//令牌签发者
+    'exp' => 60 //令牌有效期 
 ];
